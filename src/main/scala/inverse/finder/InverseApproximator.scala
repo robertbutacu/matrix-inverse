ccdddc1f680ab327e94a0526155ee552ab66a0b2
robertbutacu <robert.butacu@yahoo.com>
2018-05-01 12:49:46 +0300

    changed signature - now its correct

--- src/main/scala/inverse/finder/InverseApproximator.scala
+++ src/main/scala/inverse/finder/InverseApproximator.scala
@@ -3,19 +3,22 @@ package inverse.finder
 import matrix.RegularMatrix
 
 trait InverseApproximator[A] {
-  def computeNext(currApproximation: RegularMatrix[A]): RegularMatrix[A]
+  def computeNext(currApproximation: RegularMatrix[A], matrix: RegularMatrix[A]): RegularMatrix[A]
 }
 
-object InverseApproximator{
-  implicit def schultz[A: Numeric]: InverseApproximator[A] = (currApproximation: RegularMatrix[A]) => {
+object InverseApproximator {
+  implicit def schultz[A: Numeric]: InverseApproximator[A] =
+    (currApproximation: RegularMatrix[A], matrix: RegularMatrix[A]) => {
+      currApproximation
+    }
+
+  implicit def li[A: Numeric]: InverseApproximator[A] =
+    (currApproximation: RegularMatrix[A], matrix: RegularMatrix[A]) => {
     currApproximation
   }
 
-  implicit def li[A: Numeric]: InverseApproximator[A] = (currApproximation: RegularMatrix[A]) => {
-    currApproximation
-  }
-
-  implicit def li2[A: Numeric]: InverseApproximator[A] = (currApproximation: RegularMatrix[A]) => {
+  implicit def li2[A: Numeric]: InverseApproximator[A] =
+    (currApproximation: RegularMatrix[A], matrix: RegularMatrix[A]) => {
     currApproximation
   }
 }
