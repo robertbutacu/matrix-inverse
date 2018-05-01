953cd0b142d0c824e02c2e876a29ce675afdb11c
robertbutacu <robert.butacu@yahoo.com>
2018-05-01 13:11:47 +0300

    schultz implementation

--- src/main/scala/inverse/finder/InverseApproximator.scala
+++ src/main/scala/inverse/finder/InverseApproximator.scala
@@ -10,7 +10,12 @@ object InverseApproximator {
   implicit def schultz[A: Numeric]: InverseApproximator[A] =
     (currApproximation: RegularMatrix[A], matrix: RegularMatrix[A]) => {
       // V(k+1) = V(k)(2 In - AV(k))
-      currApproximation
+      val num = implicitly[Numeric[A]]
+      val result =
+        currApproximation.***(
+          matrix.identityMatrix.map(num.times(_, num.fromInt(2))).---(matrix.***(currApproximation)))
+
+      RegularMatrix(result.rows)
     }
 
   implicit def li[A: Numeric]: InverseApproximator[A] =
